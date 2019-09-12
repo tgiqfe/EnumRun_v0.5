55f3079b84a09c512395471266884574a9026656
tgiqfe <tqji31001@gmail.com>
2019-09-12 10:49:18 +0900

    Add v0.6

---
+++ v0.6/Manifest/PSD1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Reflection;
+using System.Diagnostics;
+
+namespace Manifest
+{
+    class PSD1
+    {
+        const string EXTENSION = ".psd1";
+
+        public static void Create(string projectName, string outputDir)
+        {
+            string dllFile = Path.Combine(outputDir, projectName + ".dll");
+            string outputFile = Path.Combine(outputDir, projectName + EXTENSION);
+            if (!File.Exists(dllFile)) { return; }
+
+            List<string> CmdletsToExport = new List<string>();
+            foreach (string csFile in Directory.GetFiles(outputDir, "*.cs", SearchOption.AllDirectories))
+            {
+                using (StreamReader sr = new StreamReader(csFile, Encoding.UTF8))
+                {
+                    string readLine = "";
+                    while ((readLine = sr.ReadLine()) != null)
+                    {
+                        if (Regex.IsMatch(readLine, @"^\s*\[Cmdlet\(Verbs"))
+                        {
+                            string cmdPre = readLine.Substring(
+                                readLine.IndexOf(".") + 1, readLine.IndexOf(",") - readLine.IndexOf(".") - 1);
+                            string cmdSuf = readLine.Substring(
+                                readLine.IndexOf("\"") + 1, readLine.LastIndexOf("\"") - readLine.IndexOf("\"") - 1);
+                            CmdletsToExport.Add(cmdPre + "-" + cmdSuf);
+                        }
+                    }
+                }
+            }
+
+            FileVersionInfo fvi = FileVersionInfo.GetVersionInfo(dllFile);
+
+            string RootModule = Path.GetFileName(dllFile);
+            string ModuleVersion = fvi.FileVersion;
+            string Guid = "75e60d76-7594-4f1b-af01-a2629646e1ec";
+            string Author = "q";
+            string CompanyName = "q";
+            string Copyright = fvi.LegalCopyright;
+            string Description = "Run enumerated script";
+
+            string manifestString = string.Format(@"@{{
+RootModule = ""{0}""
+ModuleVersion = ""{1}""
+GUID = ""{2}""
+Author = ""{3}""
+CompanyName = ""{4}""
+Copyright = ""{5}""
+Description = ""{6}""
+CmdletsToExport = @(
+""{7}""
+)
+}}",
+RootModule, ModuleVersion, Guid, Author, CompanyName, Copyright, Description,
+string.Join("\", \"", CmdletsToExport)
+);
+            using (StreamWriter sw = new StreamWriter(outputFile, false, Encoding.UTF8))
+            {
+                sw.WriteLine(manifestString);
+            }
+        }
+
+        public static void Create(string dllFile, string cmdletDir, string outputFile)
+        {
+            //  CmdletsToExportの為のコマンドレットの一覧を取得
+            List<string> CmdletsToExport = new List<string>();
+            foreach (string csFile in Directory.GetFiles(cmdletDir, "*.cs", SearchOption.AllDirectories))
+            {
+                using (StreamReader sr = new StreamReader(csFile, Encoding.UTF8))
+                {
+                    string readLine = "";
+                    while ((readLine = sr.ReadLine()) != null)
+                    {
+                        if (Regex.IsMatch(readLine, @"^\s*\[Cmdlet\(Verbs"))
+                        {
+                            string cmdPre = readLine.Substring(
+                                readLine.IndexOf(".") + 1, readLine.IndexOf(",") - readLine.IndexOf(".") - 1);
+                            string cmdSuf = readLine.Substring(
+                                readLine.IndexOf("\"") + 1, readLine.LastIndexOf("\"") - readLine.IndexOf("\"") - 1);
+                            CmdletsToExport.Add(cmdPre + "-" + cmdSuf);
+                        }
+                    }
+                }
+            }
+
+            FileVersionInfo fvi = FileVersionInfo.GetVersionInfo(dllFile);
+
+            string RootModule = Path.GetFileName(dllFile);
+            string ModuleVersion = fvi.FileVersion;
+            string Guid = "75e60d76-7594-4f1b-af01-a2629646e1ec";
+            string Author = "q";
+            string CompanyName = "q";
+            string Copyright = fvi.LegalCopyright;
+            string Description = "Run enumerated script";
+
+            string manifestString = string.Format(@"@{{
+RootModule = ""{0}""
+ModuleVersion = ""{1}""
+GUID = ""{2}""
+Author = ""{3}""
+CompanyName = ""{4}""
+Copyright = ""{5}""
+Description = ""{6}""
+CmdletsToExport = @(
+""{7}""
+)
+}}",
+RootModule, ModuleVersion, Guid, Author, CompanyName, Copyright, Description,
+string.Join("\", \"", CmdletsToExport)
+);
+            Console.WriteLine(outputFile);
+            using (StreamWriter sw = new StreamWriter(outputFile, false, Encoding.UTF8))
+            {
+                sw.WriteLine(manifestString);
+            }
+        }
+    }
+}
---
+++ v0.6/Manifest/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Reflection;
+
+namespace Manifest
+{
+    class Program
+    {
+        const string projectName = "EnumRun";
+        const string debugDir = @"..\..\..\EnumRun\bin\Debug";
+        const string releaseDir = @"..\..\..\EnumRun\bin\Release";
+        
+        static void Main(string[] args)
+        {
+            Environment.CurrentDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+
+            PSD1.Create(projectName, debugDir);
+            PSD1.Create(projectName, releaseDir);
+            PSM1.Create(projectName, debugDir);
+            PSM1.Create(projectName, releaseDir);
+        }
+    }
+}
